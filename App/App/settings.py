ff5e3347a78089e7e27adb5e1e1809e61cd2296a
Nikita Isakov <orangggenick@mail.ru>
2017-11-02 22:02:32 +0600

    Settings are changed

--- App/App/settings.py
+++ App/App/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'Board.apps.BoardConfig',
+    'widget_tweaks',
 ]
 
 MIDDLEWARE = [
@@ -120,3 +121,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), "static")
+
+MEDIA_URL = "/media/"
+MEDIA_ROOT = os.path.join(os.path.dirname(BASE_DIR), "media_cdn")
